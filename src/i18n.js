0166e430263207b156cce068de916ff051a26296
UserToDeveloper <72664125+UserToDeveloper@users.noreply.github.com>
2020-10-16 20:22:52 +0530

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,48 +1,45 @@
 import i18n from "i18next";
-import detect from "i18next-browser-languagedetector";
-import { initReactI18next} from "recat-i18next";
+import detector from "i18next-browser-languagedetector";
+import { initReactI18next  } from "react-i18next";
 
-import translionGr from './locales/gr/translation.json';
+import translationGr from './locales/gr/translation.json';
 import translationIT from './locales/it/translation.json';
 import translationSP from './locales/sp/translation.json';
 import translationRS from './locales/rs/translation.json';
-import translationENG from './locales/eng/translation.json';
+import translationENG from './locales/en/translation.json';
 
-//translation
+//translations
 const resources = {
-    gr : {
-        translation: translionGr
+    gr: {
+        translation: translationGr
     },
-    it : {
+    it: {
         translation: translationIT
     },
-    sp : {
+    sp: {
         translation: translationSP
     },
-    rs : {
+    rs: {
         translation: translationRS
     },
-    eng : {
+    eng: {
         translation: translationENG
     }
-
-}
+};
 
 i18n
-     .use(detetctor)
-     .use(initReactI18next)
-     .init(
-         {
-             resources,
-             lng: 'en',
-             fallbackLng: 'en', // use en if detecated ln is not Available
+    .use(detector)
+    .use(initReactI18next ) // passes i18n down to react-i18next
+    .init({
+        resources,
+        lng: "en",
+        fallbackLng: "en", // use en if detected lng is not available
 
-             keySeparator: 'false', // we don not use keys in form messages.welcome
+        keySeparator: false, // we do not use keys in form messages.welcome
 
-             interpolation: {
-                 ecsapvalue: 'false' // react alredy safes from xss             }
-         }
-        });
+        interpolation: {
+        escapeValue: false // react already safes from xss
+        }
+    });
 
- export default i18n;
-
+export default i18n;
